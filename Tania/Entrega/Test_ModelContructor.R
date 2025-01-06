18dc3f1459327441f911dd7b67b24e0670cc1d1e
taniaG02 <tamancho12@gmail.com>
2025-01-06 12:14:58 +0100

    Actualizado ficheros test y rmd oMHN

--- Tania/Entrega/Test_ModelContructor.R
+++ Tania/Entrega/Test_ModelContructor.R
@@ -37,22 +37,6 @@ test_random_theta_omega()
 # Q.Subdiag fuction:
 Q.Subdiag(Theta, i = 2)
 
-# Test para Q.Subdiag
-test_q_subdiag <- function() {
-  Theta <- matrix(c(0.5, 0.2, 0.3, 0.7, 0.4, 0.6), nrow = 3, ncol = 3)
-  result <- Q.Subdiag(Theta, 2)
-  
-  # Verificar que la longitud del resultado sea la correcta
-  stopifnot(length(result) == 3)
-  
-  # Verificar que la tasa base s esté correcta
-  stopifnot(result[1] == exp(Theta[2, 2]))
-  
-  print("Test Q.Subdiag pasó con éxito.")
-}
-test_q_subdiag()
-
-
 # Build.Q fuction:
 Build.Q(Theta)
 
